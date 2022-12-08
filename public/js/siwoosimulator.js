9a9454a376f677175e2956343c0aa12cc138cedc
종서최 <jongseochoe@jongseochoeui-iMac.local>
2022-12-08 16:34:56 +0900

    fgjhggvb

--- public/js/siwoosimulator.js
+++ public/js/siwoosimulator.js
@@ -4,44 +4,24 @@ const $content_field = document.querySelector('#content-field')
 const $select_background = document.querySelector('#select-background')
 const $menu1 = document.querySelector('#menu1')
 const $menu2 = document.querySelector('#menu2')
+const $button_textbox_background = document.querySelector('#button-textbox-background')
 
-
-
-$textbox_background.style.visibility = 'visible'
-$name_field.style.visibility = 'visible'
-$content_field.style.visibility = 'visible'
-$select_background.style.visibility = 'hidden'
-$menu1.style.visibility = 'hidden'
-$menu2.style.visibility = 'hidden'
-
-
-
-function preSelect() {
-    $select_background.style.visibility = 'visible'
-    $menu1.style.visibility = 'visible'
-    $menu2.style.visibility = 'visible'
-}
-
-function postSelect() {
-    $select_background.style.visibility = 'hidden'
-    $menu1.style.visibility = 'hidden'
-    $menu2.style.visibility = 'hidden'
-}
-
-function getTextToContentField(text) {
-    preSelect()
-    $content_field.textContent = text
-    postSelect()
-}
-
-$textbox_background.addEventListener('click', nextOrder)
 
 
 let indexForNextOrder = 0
 function nextOrder() {
+
     $content_field.textContent = orderList[indexForNextOrder]
-    index
+    indexForNextOrder ++
 }
+
+
+$button_textbox_background.addEventListener('click', () => {
+    alert('cdsaasdc')
+})
+
+
+
 
 const orderList = [
     '2022년 8월 9일 오후 4시 16분. 3.53kg의 최시우가 태어났습니다',
